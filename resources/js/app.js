0a452457b291c99e6a8a52a5617ca82d9c89257a
Tim Van Dijck <tim.van.dijck@gmail.com>
2025-01-10 15:05:12 +0100

    Fix clicking on tag

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,3 @@
 import ThemeSwitcher from './components/ThemeSwitcher';
 
 new ThemeSwitcher();
-
-// document.addEventListener("livewire:navigated", () => {
-//     // Add data-scroll-x to body:
-//     document.body.setAttribute("data-scroll-x", window.scrollX);
-// });
