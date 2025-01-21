1d49f34818695267d211c3c6e6ce509c4a3891a2
riasvdv <riasvdv@users.noreply.github.com>
2025-01-21 14:37:58 +0000

    Fix styling

--- app/Sheets/OperatorsPathParser.php
+++ app/Sheets/OperatorsPathParser.php
@@ -7,15 +7,14 @@ use Spatie\Sheets\PathParser;
 
 class OperatorsPathParser implements PathParser
 {
-
     public function parse(string $path): array
     {
         $parts = explode('-', substr($path, 0, -3), 3);
 
         return [
             'category' => $parts[0],
-            'index' => (int) ($parts[1] ?? throw new Exception('Invalid path: ' . $path)),
-            'slug' => $parts[2] ?? throw new Exception('Missing slug: ' . $path),
+            'index' => (int) ($parts[1] ?? throw new Exception('Invalid path: '.$path)),
+            'slug' => $parts[2] ?? throw new Exception('Missing slug: '.$path),
         ];
     }
 }
