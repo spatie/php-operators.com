7e7b4efd71c55304bf281ac39257b5de8a0b1875
Rias <hey@rias.be>
2025-01-21 16:27:18 +0100

    action helper for route

--- resources/views/components/operator.blade.php
+++ resources/views/components/operator.blade.php
@@ -1,6 +1,6 @@
 <a
     wire:navigate.hover
-    href="/operators/{{ $operator->slug }}"
+    href="{{ action(\App\Livewire\Operators::class, $operator->slug) }}"
     {{ $attributes->twMerge('flex items-center px-2 h-8 text-xs font-medium rounded-md leading-tight transition') }}
 >
     {{ $operator->title }}
