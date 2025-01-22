a0ddee3e91e60ddf1edd6ed8408c7a6d50e4bb0b
Rias <hey@rias.be>
2025-01-22 11:22:34 +0100

    Wrong url

--- resources/views/components/operator.blade.php
+++ resources/views/components/operator.blade.php
@@ -1,6 +1,6 @@
 <a
     wire:navigate.hover
-    href="{{ url($operator->slug) }}"
+    href="{{ url('/operators/' . $operator->slug) }}"
     {{ $attributes->twMerge('flex items-center px-2 h-8 text-xs font-medium rounded-md leading-tight transition') }}
 >
     {{ $operator->title }}
